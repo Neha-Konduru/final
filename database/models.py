22ab3255a0cf94430d9dea576935d019a96593d0
Konduru Neha <konduruneha@Kondurus-MacBook-Pro.local>
2024-03-30 15:33:39 +0530

    updated

--- database/models.py
+++ database/models.py
@@ -7,7 +7,13 @@ from flask_migrate import Migrate
 
 
 database_name= DB_NAME 
-database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,'localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
+if(database_path != ""):
+    if database_path.startswith("postgres://"):
+        database_path = database_path.replace("postgres://", "postgresql://", 1)
+else:
+    database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,'localhost:5432', database_name)
+
 
 db = SQLAlchemy()
 
@@ -19,7 +25,6 @@ class Models:
     @staticmethod
     def setup_db(app, database_path=database_path):
         with app.app_context():
-            print(database_path)
             app.config["SQLALCHEMY_DATABASE_URI"] = database_path
             app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
             db.app = app
